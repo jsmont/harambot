00c4106c94be89d2afcc504c5549cc4e21414457
jsmont <jsmont.sol@gmail.com>
2017-03-05 04:26:06 +0100

    Moriarty implemented

---
+++ moriarty.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+)
+
+func getLanguage(text string) string {
+	data := map[string]string{"textIn": text}
+	dataBytes, _ := json.Marshal(data)
+	req, _ := http.NewRequest("POST", "https://jmlk74oovf.execute-api.eu-west-1.amazonaws.com/dev/language?wait=true", bytes.NewBuffer(dataBytes))
+	req.Header.Set("x-api-key", "9CAfxmC4WB10tnS9RY9oG92Io0M4trVp7HpTUEjR")
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	jsonBody := map[string]interface{}{}
+	json.Unmarshal(body, &jsonBody)
+	return jsonBody["results"].(map[string]interface{})["language"].(string)
+}
+
+func getSentiment(text, language string) string {
+	data := map[string]string{"textIn": text, "language": language}
+	dataBytes, _ := json.Marshal(data)
+	req, _ := http.NewRequest("POST", "https://jmlk74oovf.execute-api.eu-west-1.amazonaws.com/dev/sentiment?wait=true", bytes.NewBuffer(dataBytes))
+	req.Header.Set("x-api-key", "9CAfxmC4WB10tnS9RY9oG92Io0M4trVp7HpTUEjR")
+	req.Header.Set("Content-Type", "application/json")
+	client := &http.Client{}
+	resp, _ := client.Do(req)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
+	jsonBody := map[string]interface{}{}
+	json.Unmarshal(body, &jsonBody)
+	return jsonBody["results"].(map[string]interface{})["prediction"].(string)
+}
