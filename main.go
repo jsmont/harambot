0ca89382ca597aaed51f70a4c6218be2f781f31a
jsmont <jsmont.sol@gmail.com>
2017-03-05 06:21:02 +0100

    API confirmation endpoint created

--- main.go
+++ main.go
@@ -64,8 +64,8 @@ func main() {
 		pageAccessToken = "EAACEdEose0cBAAEQQwICorU1aKIt069OejnoADBDiUIAudPaZC4SgvAf3lMLutkDFFlvmUOIH9tqw2dH4zFWCml5itx1m01L397Nf63XjAoVlooNZAupjwBLsbZCqM782tYFFNTW3odI8F8EZALT5z1Wm9JSNoj3Kqhjzlue3InU7PdyJKBn"
 	}
 
-	//go startInputService(db, pageId, pageAccessToken)
-	go startValidatorService(db)
+	go startInputService(db, pageId, pageAccessToken)
+	//go startValidatorService(db)
 
 	// add the handler
 	http.Handle("/potentiallist", Adapt(http.HandlerFunc(potentialListHandler), withDB(db), context.ClearHandler))
