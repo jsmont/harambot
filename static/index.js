692a2660e5f8d2b63957565404b4f0e249c6a2fd
Nixie <nixiescream@gmail.com>
2017-03-05 03:41:10 +0100

    changed JQuery

--- static/index.js
+++ static/index.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
     for(var i = 0; i < a.length; i++){
+        function GetFormattedDate(todayTime) {
+            var month = (todayTime.getMonth() + 1);
+            var day = (todayTime.getDate());
+            var year = (todayTime.getFullYear());
+            var hour = todayTime.getHours();
+            var minute = todayTime.getMinutes();
+            return month + "/" + day + "/" + year + " - " + hour + ":" + minute;
+        }
         var item = "<div class='comment-box col-md-6 col-xs-offset-3' data-id='1'>\
             <dl>\
-                <dt>Nom | Id | HH:MI</dt>\
-                <dd>comentari</dd>\
+                <dt>"+ a[i].username +" | "+ GetFormattedDate(a[i].timestamp) + "</dt>\
+                <dd>"+ a[i].message +"</dd>\
             </dl>\
             <div class='buttons col-md-offset-9'>\
                 <button type'button' class='btn approved btn-success'>Approved</button>\
@@ -25,18 +33,18 @@ $(document).ready(function(){
 
 a = [
     {
-    username: "Anonymous",
+    username: "Anonymous1",
     timestamp: new Date(),
-    message: "Random"
+    message: "Random1"
     },
     {
-    username: "Anonymous",
+    username: "Anonymous2",
     timestamp: new Date(),
-    message: "Random"
+    message: "Random2"
     },
     {
-    username: "Anonymous",
+    username: "Anonymous3",
     timestamp: new Date(),
-    message: "Random"
+    message: "Random3"
     }
 ];
